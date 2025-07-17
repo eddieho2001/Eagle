d2c8354f8f9a4eef0006fbba10418bdb52cbf4ee
eddieho <eddie.ho@oneport.com>
2025-07-17 17:36:05 +0800

    update Game class

--- game/include/framework/game.h
+++ game/include/framework/game.h
@@ -8,11 +8,19 @@ public:
 	void run();
 private:
 	void processEvents();
+	void handlePlayerInput(sf::Keyboard::Key key, bool isPressed);
 	void update();
 	void render();
 private:
 	sf::RenderWindow mWindow;
 	sf::CircleShape mPlayer;
 	quill::Logger* mlogger;
+	bool mIsMovingUp;
+	bool mIsMovingDown;
+	bool mIsMovingLeft;
+	bool mIsMovingRight;
+
+	sf::Vector2f mMovement;
+
 
 };
--- game/src/framework/game.cpp
+++ game/src/framework/game.cpp
@@ -6,7 +6,12 @@
 
 Game::Game()
 	:mWindow{ sf::VideoMode(640, 480), "SFML Application" },
-	mPlayer{}
+	mPlayer{},
+	mIsMovingUp{false},
+	mIsMovingDown{false},
+	mIsMovingLeft{false},
+	mIsMovingRight{false},
+	mMovement{0.f,0.f}
 {
 	mPlayer.setRadius(40.f);
 	mPlayer.setPosition(100.f, 100.f);
@@ -29,13 +34,63 @@ void Game::processEvents() {
 	sf::Event event;
 	while (mWindow.pollEvent(event))
 	{
+		/*
 		if (event.type == sf::Event::Closed)
 			mWindow.close();
+		*/
+		switch (event.type) {
+		case sf::Event::Closed:
+			mWindow.close();
+			break;
+		case sf::Event::KeyPressed:
+			handlePlayerInput(event.key.code, true);
+			break;
+		case sf::Event::KeyReleased:
+			handlePlayerInput(event.key.code, true);
+			break;
+		}
+
+	}
+}
+
+void Game::handlePlayerInput(sf::Keyboard::Key key, bool isPressed)
+{
+	if (key == sf::Keyboard::W || key == sf::Keyboard::PageUp) {
+		mIsMovingUp = isPressed;
+		LOG_INFO(mlogger, "Move Up");
+	}
+	else if (key == sf::Keyboard::S|| key == sf::Keyboard::PageDown) {
+		mIsMovingDown = isPressed;
+		LOG_INFO(mlogger, "Move Down");
+	}
+	else if (key == sf::Keyboard::A || key == sf::Keyboard::Left) {
+		mIsMovingLeft = isPressed;
+		LOG_INFO(mlogger, "Move Left");
+	}
+	else if (key == sf::Keyboard::D || key == sf::Keyboard::Right) {
+		mIsMovingRight = isPressed;
+		LOG_INFO(mlogger, "Move Right");
 	}
 }
 
 void Game::update() {
+	if (mIsMovingUp) {
+		mMovement.y -= 1.f;
+	}
 
+	if (mIsMovingDown) {
+		mMovement.y += 1.f;
+	}
+
+	if (mIsMovingLeft) {
+		mMovement.x += 1.f;
+	}
+
+	if (mIsMovingRight) {
+		mMovement.x -= 1.f;
+	}
+
+	mPlayer.move(mMovement );
 }
 
 void Game::render() {
